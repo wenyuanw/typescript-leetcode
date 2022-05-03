1a7b9656f9d34d3c443266cc35bd30abe184b841
wenyuan <wenyuan19@163.com>
2022-05-03 13:03:41 +0800

    feat: 14-最长公共前缀

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # typescript-leetcode
 以前刷题都是使用 JavaScript，作为一名前端开发工程师，TypeScript 已经是必备技能了，因此决定今后刷题的时候使用 TypeScript，进一步掌握 TypeScript 的使用。
 
+# TODO
+1. commit lint 的添加
+2. 
--- leetcode/14-最长公共前缀.ts
+++ leetcode/14-最长公共前缀.ts
@@ -1,3 +1,16 @@
 function longestCommonPrefix(strs: string[]): string {
-  
+  if (strs.length === 0) return ''
+  let ans = strs[0]
+  for (let i = 1; i < strs.length; i++) {
+    const len = Math.min(ans.length, strs[i].length)
+    let count = 0
+    for (; count < len; count++) {
+      if (ans[count] !== strs[i][count]) {
+        break
+      }
+    }
+    ans = ans.substring(0, count)
+    if (ans === '') return ans
+  }
+  return ans
 }
